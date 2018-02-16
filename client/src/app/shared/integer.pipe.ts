3d41c8966e63555000947e110c1792afe0970746
Roland Vachter <roland.vachter@ft.com>
2018-02-16 09:10:02 +0200

    Fix determining integer value.

--- client/src/app/shared/integer.pipe.ts
+++ client/src/app/shared/integer.pipe.ts
@@ -5,14 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IntegerPipe implements PipeTransform {
 
-	transform(value: number):any {		
+	transform(value: number):any {
 		if (isNaN(value)) {
 			return '-';
 		}
 
-		const intValue = value < 0 ? Math.ceil(value) : Math.floor(value);
+		let intValue = value < 0 ? Math.ceil(value) : Math.floor(value);
 
-		return value < 0 && value > -1 ? '-' + intValue : intValue;
+		const absValue = Math.abs(value);
+		const decimal = parseInt(((absValue - Math.floor(absValue)) * 10).toFixed(0), 10);
+
+		if (decimal === 10) {
+			if (value > 0) {
+				intValue += 1;
+			} else {
+				intValue -= 1;
+			}
+		}
+
+		return intValue < 0 && intValue > -1 ? '-' + intValue : intValue;
 	}
 
 }
