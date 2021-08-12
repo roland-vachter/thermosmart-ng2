3310233cc954a62c047fddddac260c2e2e941b06
Roland Vachter <roland.vachter@iquestgroup.com>
2021-08-12 19:32:17 +0300

    Add missing file.

---
+++ server/lib/services/pushNotifications.js
@@ -0,0 +1,24 @@
+const PushNotifications = require('@pusher/push-notifications-server');
+
+let pushNotifications = new PushNotifications({
+	instanceId: process.env.PUSHER_INSTANCE_ID,
+	secretKey: process.env.PUSHER_SECRET_KEY,
+});
+
+exports.send = (interests, title, body) => {
+	pushNotifications
+		.publishToInterests(interests, {
+			fcm: {
+				notification: {
+					title,
+					body
+				},
+			},
+		})
+	.then(publishResponse => {
+		console.log('Pusher notification sent:', publishResponse.publishId);
+	})
+	.catch(error => {
+		console.log('Pusher error:', error);
+	});
+}
