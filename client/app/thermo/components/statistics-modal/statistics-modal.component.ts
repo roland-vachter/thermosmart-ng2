b8160acd7aa4ed41903c9bb2b466187e8722b814
Vachter Roland <roland.vachter@nagarro.com>
2023-04-06 11:59:27 +0300

    Fix syntax error.

--- client/app/thermo/components/statistics-modal/statistics-modal.component.ts
+++ client/app/thermo/components/statistics-modal/statistics-modal.component.ts
@@ -147,7 +147,7 @@ export class StatisticsModalComponent implements OnInit {
 								pointHoverBackgroundColor: `rgba(${this.colors[1][0]},${this.colors[1][1]},${this.colors[1][2]},1)`,
 								pointHoverBorderColor: "rgba(220,220,220,1)",
 								pointHoverBorderWidth: 2,
-								pointRadius: 3
+								pointRadius: 3,
 								fill: false,
 							}, {
 								label: 'Avg outside temperature',
@@ -167,7 +167,7 @@ export class StatisticsModalComponent implements OnInit {
 								pointHoverBackgroundColor: `rgba(${this.colors[2][0]},${this.colors[2][1]},${this.colors[2][2]},1)`,
 								pointHoverBorderColor: "rgba(220,220,220,1)",
 								pointHoverBorderWidth: 2,
-								pointRadius: 3
+								pointRadius: 3,
 								fill: false,
 							}]
 						},
@@ -294,7 +294,7 @@ export class StatisticsModalComponent implements OnInit {
 								pointHoverBackgroundColor: `rgba(${this.colors[1][0]},${this.colors[1][1]},${this.colors[1][2]},1)`,
 								pointHoverBorderColor: "rgba(220,220,220,1)",
 								pointHoverBorderWidth: 2,
-								pointRadius: 3
+								pointRadius: 3,
 								fill: false,
 							}, {
 								label: 'Avg outside temperature',
@@ -314,7 +314,7 @@ export class StatisticsModalComponent implements OnInit {
 								pointHoverBackgroundColor: `rgba(${this.colors[2][0]},${this.colors[2][1]},${this.colors[2][2]},1)`,
 								pointHoverBorderColor: "rgba(220,220,220,1)",
 								pointHoverBorderWidth: 2,
-								pointRadius: 3
+								pointRadius: 3,
 								fill: false,
 							}]
 						},
@@ -428,7 +428,7 @@ export class StatisticsModalComponent implements OnInit {
 								pointHoverBackgroundColor: `rgba(${this.colors[index % this.colors.length][0]},${this.colors[index % this.colors.length][1]},${this.colors[index % this.colors.length][2]},1)`,
 								pointHoverBorderColor: "rgba(220,220,220,1)",
 								pointHoverBorderWidth: 2,
-								pointRadius: 3
+								pointRadius: 3,
 								fill: false,
 								tension: 0.3
 							}))
