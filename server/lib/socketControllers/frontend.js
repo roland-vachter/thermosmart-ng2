554c0f02a2d03c5919e46157b18192a6b1753bc3
Vachter Roland <roland.vachter@nagarro.com>
2023-04-13 14:50:42 +0300

    Re-enable socket update.

--- server/lib/socketControllers/frontend.js
+++ server/lib/socketControllers/frontend.js
@@ -21,209 +21,209 @@ let initialized = false;
 
 exports.init = function () {
 	if (!initialized) {
-		// initialized = true;
+		initialized = true;
 
-		// const io = socket.io.of('/frontend');
-		// Location.find().lean().exec().then(locations => {
-		// 	locations.forEach(l => socket.io.of(`/frontend/${l._id}`));
-		// });
+		const io = socket.io.of('/frontend');
+		Location.find().lean().exec().then(locations => {
+			locations.forEach(l => socket.io.of(`/frontend/${l._id}`));
+		});
 
-		// const recalculateHeatingDuration = (location) => {
-		// 	Promise.all([
-		// 		statisticsService
-		// 			.getStatisticsForToday(location)
-		// 	]).then(results => {
-		// 		const [
-		// 			statisticsForToday
-		// 		] = results;
+		const recalculateHeatingDuration = (location) => {
+			Promise.all([
+				statisticsService
+					.getStatisticsForToday(location)
+			]).then(results => {
+				const [
+					statisticsForToday
+				] = results;
 
-		// 		socket.io.of('/frontend/' + location).emit('update', {
-		// 			statisticsForToday: statisticsForToday
-		// 		});
-		// 	});
-		// };
+				socket.io.of('/frontend/' + location).emit('update', {
+					statisticsForToday: statisticsForToday
+				});
+			});
+		};
 
-		// // heating
+		// heating
 
-		// outsideConditions.evts.on('change', data => {
-		// 	io.emit('update', {
-		// 		outside: data
-		// 	});
-		// });
+		outsideConditions.evts.on('change', data => {
+			io.emit('update', {
+				outside: data
+			});
+		});
 
-		// insideConditions.evts.on('change', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		sensor: data
-		// 	});
-		// });
+		insideConditions.evts.on('change', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				sensor: data
+			});
+		});
 
-		// heatingEvts.on('changeHeating', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		isHeatingOn: data.isOn
-		// 	});
+		heatingEvts.on('changeHeating', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				isHeatingOn: data.isOn
+			});
 
-		// 	recalculateHeatingDuration();
-		// });
+			recalculateHeatingDuration();
+		});
 
-		// heatingEvts.on('changeHeatingPower', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		heatingPower: {
-		// 			status: data.poweredOn,
-		// 			until: data.until
-		// 		}
-		// 	});
-		// });
+		heatingEvts.on('changeHeatingPower', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				heatingPower: {
+					status: data.poweredOn,
+					until: data.until
+				}
+			});
+		});
 
-		// restartSensorService.evts.on('change', data => {
-		// 	io.emit('update', {
-		// 		restartSensorInProgress: data
-		// 	});
-		// });
+		restartSensorService.evts.on('change', data => {
+			io.emit('update', {
+				restartSensorInProgress: data
+			});
+		});
 
-		// config.evts.on('change', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		config: data.config
-		// 	});
-		// });
+		config.evts.on('change', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				config: data.config
+			});
+		});
 
-		// targetTempService.evts.on('change', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		targetTempId: data.targetTemp.id
-		// 	})
-		// });
+		targetTempService.evts.on('change', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				targetTempId: data.targetTemp.id
+			})
+		});
 
-		// Temperature.evts.on('change', data => {
-		// 	let ids = data.ids;
-		// 	if (!(data.ids instanceof Array)) {
-		// 		ids = [data.ids];
-		// 	}
+		Temperature.evts.on('change', data => {
+			let ids = data.ids;
+			if (!(data.ids instanceof Array)) {
+				ids = [data.ids];
+			}
 
-		// 	Temperature.find({
-		// 		_id: {
-		// 			$in: ids
-		// 		}
-		// 	}).lean().exec().then(temps => {
-		// 		socket.io.of('/frontend/' + data.location).emit('update', {
-		// 			temperatures: temps.map(t => ({
-		// 				...t,
-		// 				value: t.values?.find(v => v.location === data.location)?.value || t.defaultValue,
-		// 				values: null
-		// 			}))
-		// 		});
-		// 	});
-		// });
+			Temperature.find({
+				_id: {
+					$in: ids
+				}
+			}).lean().exec().then(temps => {
+				socket.io.of('/frontend/' + data.location).emit('update', {
+					temperatures: temps.map(t => ({
+						...t,
+						value: t.values?.find(v => v.location === data.location)?.value || t.defaultValue,
+						values: null
+					}))
+				});
+			});
+		});
 
-		// HeatingDefaultPlan.evts.on('change', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		heatingDefaultPlans: [{
-		// 			...data.defaultPlan,
-		// 			plan: data.defaultPlan.plans?.find(p => p.location === data.location)?.plan || data.defaultPlan.defaultPlan,
-		// 			plans: null
-		// 		}]
-		// 	});
-		// });
+		HeatingDefaultPlan.evts.on('change', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				heatingDefaultPlans: [{
+					...data.defaultPlan,
+					plan: data.defaultPlan.plans?.find(p => p.location === data.location)?.plan || data.defaultPlan.defaultPlan,
+					plans: null
+				}]
+			});
+		});
 
-		// HeatingPlanOverrides.evts.on('change', data => {
-		// 	HeatingPlanOverrides
-		// 		.find()
-		// 		.sort({
-		// 			date: 1,
-		// 			location: data.location
-		// 		})
-		//		.lean()
-		// 		.exec()
-		// 		.then(planOverrides => {
-		// 			socket.io.of('/frontend/' + data.location).emit('update', {
-		// 				HeatingPlanOverrides: planOverrides
-		// 			})
-		// 		});
-		// })
-
-
-		// setInterval(() => {
-		// 	Location.find().lean().exec().then(locations => {
-		// 		locations.forEach(l => {
-		// 			if (heatingService.isHeatingOn(l._id)) {
-		// 				recalculateHeatingDuration(l._id);
-		// 			}
-		// 		});
-		// 	});
-		// }, 5 * 60 * 1000);
+		HeatingPlanOverrides.evts.on('change', data => {
+			HeatingPlanOverrides
+				.find()
+				.sort({
+					date: 1,
+					location: data.location
+				})
+				.lean()
+				.exec()
+				.then(planOverrides => {
+					socket.io.of('/frontend/' + data.location).emit('update', {
+						HeatingPlanOverrides: planOverrides
+					})
+				});
+		})
 
 
-
-		// // security
-		// securityStatus.evts.on('status', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		security: {
-		// 			status: data.status
-		// 		}
-		// 	});
-		// });
-
-		// securityStatus.evts.on('alarm', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		security: {
-		// 			alarm: data.on
-		// 		}
-		// 	});
-		// });
-
-		// securityStatus.evts.on('movement', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		security: {
-		// 			movement: true
-		// 		}
-		// 	});
-		// });
-
-		// securityStatus.evts.on('alarmTriggeredCount', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		security: {
-		// 			alarmTriggeredCount: data.count
-		// 		}
-		// 	})
-		// });
-
-		// securityHealth.evts.on('camera-health', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		security: {
-		// 			cameraHealth: data.health
-		// 		}
-		// 	})
-		// });
-
-		// securityHealth.evts.on('controller-health', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		security: {
-		// 			controllerHealth: data.health
-		// 		}
-		// 	})
-		// });
-
-		// securityHealth.evts.on('keypad-health', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		security: {
-		// 			keypadHealth: data.health
-		// 		}
-		// 	})
-		// });
-
-		// securityHealth.evts.on('motion-sensor-health', data => {
-		// 	socket.io.of('/frontend/' + data.location).emit('update', {
-		// 		security: {
-		// 			motionSensorHealth: data.health
-		// 		}
-		// 	})
-		// });
+		setInterval(() => {
+			Location.find().lean().exec().then(locations => {
+				locations.forEach(l => {
+					if (heatingService.isHeatingOn(l._id)) {
+						recalculateHeatingDuration(l._id);
+					}
+				});
+			});
+		}, 5 * 60 * 1000);
 
 
-		// plantWatering.evts.on('change', data => {
-		// 	io.emit('update', {
-		// 		plantWatering: {
-		// 			status: data
-		// 		}
-		// 	})
-		// });
+
+		// security
+		securityStatus.evts.on('status', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				security: {
+					status: data.status
+				}
+			});
+		});
+
+		securityStatus.evts.on('alarm', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				security: {
+					alarm: data.on
+				}
+			});
+		});
+
+		securityStatus.evts.on('movement', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				security: {
+					movement: true
+				}
+			});
+		});
+
+		securityStatus.evts.on('alarmTriggeredCount', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				security: {
+					alarmTriggeredCount: data.count
+				}
+			})
+		});
+
+		securityHealth.evts.on('camera-health', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				security: {
+					cameraHealth: data.health
+				}
+			})
+		});
+
+		securityHealth.evts.on('controller-health', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				security: {
+					controllerHealth: data.health
+				}
+			})
+		});
+
+		securityHealth.evts.on('keypad-health', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				security: {
+					keypadHealth: data.health
+				}
+			})
+		});
+
+		securityHealth.evts.on('motion-sensor-health', data => {
+			socket.io.of('/frontend/' + data.location).emit('update', {
+				security: {
+					motionSensorHealth: data.health
+				}
+			})
+		});
+
+
+		plantWatering.evts.on('change', data => {
+			io.emit('update', {
+				plantWatering: {
+					status: data
+				}
+			})
+		});
 	}
 };
